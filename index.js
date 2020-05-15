f24e32dc2a33031eda7794e2b862555ae143f3a6
SebastianGarces <gsgarces@gmail.com>
2020-05-15 02:05:56 -0400

    brought in node-api3-project

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require("./server");
 
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => console.log(`Server listening on localhost:${PORT}`));
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const helmet = require("helmet");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(logger);
+server.use("/api/users", userRouter);
 
 server.get("/", (req, res) => {
-	res.status(200).json({ api: "up" });
+	res.send(`<h1 style='font-size:100px'>Node API 4 Project</h1>`);
 });
+
+//custom middleware
+
+function logger(req, res, next) {
+	console.log(req.method + " Request");
+	next();
+}
 
 module.exports = server;
